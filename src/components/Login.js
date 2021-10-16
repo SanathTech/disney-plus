48388cb3f3e1cd53356fc2ebd918c9695d4c3846
Sanath <sanathpun@hotmail.com>
2021-10-16 11:17:58 +1100

    added rest of home page components

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,13 @@ const Login = (props) => {
             <Content>
                 <CTA>
                     <CTALogoOne src="/disney-plus/images/cta-logo-one.svg" alt=""/>
+                    <SignUp>SIGN UP NOW</SignUp>
+                    <Description>
+                        Get Premier Access to Raya and the Last Dragon for an additional fee
+                        with a Disney+ subscription. As of 03/26/21, the price of Disney+
+                        and The Disney Bundle will increase by $1.
+                    </Description>
+                    <CTALogoTwo src="/disney-plus/images/cta-logo-two.png" alt=""/>
                 </CTA>
                 <BgImage />
             </Content>
@@ -50,10 +57,11 @@ const BgImage = styled.div`
 `;
 
 const CTA = styled.div`
-    max-width: 650px;
+    max-width: 600px;
     width: 100%;
     display: flex;
     flex-direction: column;
+    align-items: center;
 `;
 
 const CTALogoOne = styled.img`
@@ -61,6 +69,38 @@ const CTALogoOne = styled.img`
     max-width: 600px;
     min-height: 1px;
     display: block;
+    width: 100%;
+`;
+
+const SignUp = styled.a`
+    color: #f9f9f9;
+    background-color: #0063e5;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spacing: 1px;
+    font-size: 18px;
+    padding: 16.5px 0;
+    border: 1px solid transparent;
+    border-radius: 4px;
+
+    &:hover {
+        background-color: #0483ee;
+    }
+`
+
+const Description = styled.p`
+    color: hsla(0,0%,95.3%,1);
+    font-size: 11px;
+    margin: 0 0 24px;
+    line-height: 1.5;
+    letter-spacing: 1px;
+`
+
+const CTALogoTwo = styled.img`
+    max-width: 600px;
+    margin-bottom: 20px;
+    display: inline-block;
+    vertical-align: bottom;
     width: 100%;
 `;
 
